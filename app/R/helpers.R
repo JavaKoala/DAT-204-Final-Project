6c45857649059091237e5740c9ebf96f38a756ec
Ben Meyer <benmeyer@bpmeyer.com>
2022-11-19 11:14:10 -0500

    Refactor latest_on_time_percent to use pipes

--- app/R/helpers.R
+++ app/R/helpers.R
@@ -1,13 +1,11 @@
 # function to return the latest on time percent
+# used by the route-module to find the last on time percent
 latest_on_time_percent <- function(dataset, route, route_day_type) {
   route_name <- route
   latest_percent <- slice_head(
-    arrange(
-      filter(
-        dataset,
-        route_full_name == route_name & day_type == route_day_type),
-      desc(month_start)
-    )
+    dataset %>%
+      filter(route_full_name == route_name & day_type == route_day_type) %>%
+      arrange(desc(month_start))
   )['on_time_percent'] * 100
   
   if (toString(latest_percent) == "numeric(0)") {
