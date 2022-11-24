ea1ac8229e09538d7536ded8852357406d04c04f
Ben Meyer <benmeyer@bpmeyer.com>
2022-11-24 11:04:20 -0500

    Refactor helpers into route module

--- app/R/route-module.R
+++ app/R/route-module.R
@@ -3,6 +3,44 @@
 # The latest month's on time percentage for each day type is displayed
 # A scatter plot of the on time percentage over time is displayed
 #   with different colors for each day type
+
+# function to return the list of distinct routes
+# used by the route-module for the route selector
+distinct_routes <- function(dataset) {
+  distinct(dataset['route_full_name'])[[1]]
+}
+
+# function used to return the distinct day types
+# used by the route-module for the day type options
+day_types <- function(dataset) {
+  distinct(dataset['day_type'])[[1]]
+}
+
+# function to return the latest on time percent
+# used by the route-module to find the last on time percent
+latest_on_time_percent <- function(dataset, route, route_day_type) {
+  route_name <- route
+  latest_percent <- slice_head(
+    dataset %>%
+      filter(route_full_name == route_name & day_type == route_day_type) %>%
+      arrange(desc(month_start))
+  )['on_time_percent'] * 100
+
+  if (toString(latest_percent) == "numeric(0)") {
+    return("NA")
+  } else {
+    return(paste0(toString(latest_percent), "%"))
+  }
+}
+
+# function to return the data used for the route module graphs
+route_module_data <- function(dataset, route, day_types) {
+  route_name <- route
+  dataset %>%
+    filter(route_full_name == route_name & day_type %in% day_types) %>%
+    mutate(on_time_percent_100 = on_time_percent * 100)
+}
+
 routeModuleUI <- function(id, dataset) {
   # All uses of Shiny input/output IDs in the UI must be namespaced
   ns <- NS(id)
--- app/tests/testthat/test-route-module.R
+++ app/tests/testthat/test-route-module.R
@@ -1,5 +1,85 @@
 test_dataset <- read_csv('../test_data/dataset.csv')
 
+# Test that the distinct_routes function returns a list of routes ordered
+# by route number
+test_that("a list of routes is returned", {
+  test_routes <- distinct_routes(test_dataset)
+
+  expect_equal(test_routes[1], "1 - FREEPORT ROAD")
+  expect_equal(test_routes[104], "SLVR - Libary via Overbrook")
+})
+
+# Test that the day_types method returns the distinct day types
+test_that("the route types are returned", {
+  test_day_types <- day_types(test_dataset)
+
+  expect_equal(test_day_types[1], "WEEKDAY")
+  expect_equal(test_day_types[2], "SAT.")
+  expect_equal(test_day_types[3], "SUN.")
+})
+
+# Test that the latest_on_time_percent function returns the latest percent
+# for a given route and day type
+test_that("the latest on time percent is returned", {
+  expect_equal(
+    latest_on_time_percent(test_dataset, '31 - BRIDGEVILLE', 'WEEKDAY'),
+    "68.56%"
+  )
+  expect_equal(
+    latest_on_time_percent(test_dataset, '31 - BRIDGEVILLE', 'SAT.'),
+    "62.7%"
+  )
+})
+
+test_that("NA is returned for an invalid route", {
+  expect_equal(
+    latest_on_time_percent(test_dataset, 'INVALID', 'WEEKDAY'),
+    "NA"
+  )
+})
+
+test_that("NA is returned for an invalid day type", {
+  expect_equal(
+    latest_on_time_percent(test_dataset, '31 - BRIDGEVILLE', 'INVALID'),
+    "NA"
+  )
+})
+
+# Test that the route_module_data function returns the data set filtered by
+# route and day types
+test_that("the route_module_data filters data by route", {
+  filtered_data <- route_module_data(
+    test_dataset,
+    "6 - SPRING HILL",
+    day_types(test_dataset)
+  )
+  distinct_route <- distinct(filtered_data['route_full_name'])[[1]]
+
+  expect_equal(distinct_route, "6 - SPRING HILL")
+})
+
+test_that("the route_module_data filters data by day type", {
+  filtered_data <- route_module_data(
+    test_dataset,
+    "8 - PERRYSVILLE",
+    c("SAT.")
+  )
+  distinct_day_type <- distinct(filtered_data['day_type'])[[1]]
+
+  expect_equal(distinct_day_type, "SAT.")
+})
+
+test_that("the route_module_data has the precent multiplied by 100", {
+  filtered_data <- route_module_data(
+    test_dataset,
+    "11 - FINEVIEW",
+    day_types(test_dataset)
+  )
+  test_percent <- slice_head(filtered_data)['on_time_percent_100'][[1]]
+
+  expect_equal(test_percent, 74.38)
+})
+
 test_that("the latest on time percents are returned", {
   testServer(
     routeModuleServer,
@@ -23,7 +103,7 @@ test_that("confirm the on time percent plot can be accessed without error", {
       # Set the route and the day type
       session$setInputs(route = "2 - MOUNT ROYAL")
       session$setInputs(day_types = day_types(test_dataset))
-      
+
       expect_type(output$onTimePercentPlot, "list")
     }
   )
