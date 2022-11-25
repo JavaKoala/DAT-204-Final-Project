7bffb8d95fcd32d6a3458abeb72202b477251b66
Ben Meyer <benmeyer@bpmeyer.com>
2022-11-25 14:09:53 -0500

    Add mean bus on type precent plot

--- app/R/aggregate-module.R
+++ app/R/aggregate-module.R
@@ -6,6 +6,15 @@ mean_on_time_by_mode <- function(dataset) {
     summarize(mean_on_time_percent = mean(on_time_percent_100, na.rm = TRUE))
 }
 
+# function to return the mean bus on time percent by day type
+mean_bus_on_time_percent_by_day_type <- function(dataset) {
+  dataset %>%
+    filter(mode == "Bus") %>%
+    mutate(on_time_percent_100 = on_time_percent * 100) %>%
+    group_by(day_type, month_start) %>%
+    summarize(mean_on_time_percent = mean(on_time_percent_100, na.rm = TRUE))
+}
+
 # This module is used to display aggregate data
 aggregateModuleUI <- function(id) {
   ns <- NS(id)
@@ -17,6 +26,12 @@ aggregateModuleUI <- function(id) {
         time percentages."),
     p("NA are observations where the mode is not defined."),
     plotOutput(outputId = ns("meanOnTimePercentByMode")),
+
+    h4("Graph 4: Mean Bus On Time Percent By Day Type"),
+    p("This graph is shows the differences in on time percentages by day type"),
+    p("The dip in SAT. and SUN. appear to correspond to the switch to Celver"),
+    plotOutput(outputId = ns("meanBusOnTimePercentByDayType")),
+
     width = 12
   )
 }
@@ -33,6 +48,22 @@ aggregateModuleServer <- function(id, dataset) {
           aes(month_start, mean_on_time_percent, color = mode)) +
           geom_line(size = 1) +
           ggtitle("Mean On Time Percent By Transportation Mode") +
+          scale_x_date(date_breaks = "6 month", date_labels = "%m/%y") +
+          xlab("Month") +
+          ylab("Mean On Time Percent") +
+          theme(
+            plot.title = element_text(hjust = 0.5, size = 16),
+            axis.title = element_text(size = 14)
+          )
+      })
+
+      # Plot of the mean on-time percent over time vs mode
+      output$meanBusOnTimePercentByDayType <- renderPlot({
+        ggplot(
+          data = mean_bus_on_time_percent_by_day_type(dataset),
+          aes(month_start, mean_on_time_percent, color = day_type)) +
+          geom_line(size = 1) +
+          ggtitle("Mean Bus On Time Percent By Day Type") +
           scale_x_date(date_breaks = "6 month", date_labels = "%m/%y") +
           xlab("Month") +
           ylab("Mean On Time Percent") +
--- app/tests/testthat/test-aggregate-module.R
+++ app/tests/testthat/test-aggregate-module.R
@@ -14,6 +14,23 @@ test_that("the data is aggregated by route and month start", {
   expect_equal(rounded_mean, c(66.25, 80.86))
 })
 
+# Test that the mean_bus_on_time_percent_by_day_type
+# returns the mean Bus on time percent by day type
+test_that("the data is aggregated by day type and month start", {
+  aggregate_data <- mean_bus_on_time_percent_by_day_type(test_dataset)
+  single_month <- aggregate_data %>%
+    filter(month_start == "2019-05-01")
+  start_month <- as.Date("2019-05-01")
+  rounded_mean <- round(single_month$mean_on_time_percent, 2)
+
+  print(rounded_mean)
+  expect_equal(single_month$day_type, c("SAT.", "SUN.", "WEEKDAY"))
+  expect_equal(
+    single_month$month_start, c(start_month, start_month, start_month)
+  )
+  expect_equal(rounded_mean, c(65.61, 70.13, 64.37))
+})
+
 # Test server
 test_that("on time percent by route plot can be accessed without error", {
   testServer(
