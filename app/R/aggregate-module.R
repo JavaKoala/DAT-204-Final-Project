9c5efb45bdced5143f74a541f20bb36d756b3e16
Ben Meyer <benmeyer@bpmeyer.com>
2022-12-07 23:05:23 -0500

    Add 0 percent toggle for aggregate model

--- app/R/aggregate-module.R
+++ app/R/aggregate-module.R
@@ -1,25 +1,47 @@
 # function to return the mean on time percent by mode
-mean_on_time_by_mode <- function(dataset) {
+mean_on_time_by_mode <- function(dataset, include_zero) {
   dataset %>%
     filter(mode != "NA") %>%
+    filter(
+      if (include_zero == 1) {
+        on_time_percent >= 0
+      } else {
+        on_time_percent > 0
+      }
+    ) %>%
     mutate(on_time_percent_100 = on_time_percent * 100) %>%
     group_by(mode, month_start) %>%
     summarize(mean_on_time_percent = mean(on_time_percent_100, na.rm = TRUE))
 }
 
 # function to return the mean mode on time percent by day type
-mean_mode_on_time_percent_by_day_type <- function(dataset, trans_mode) {
+mean_mode_on_time_percent_by_day_type <- function(
+    dataset, trans_mode, include_zero) {
   dataset %>%
     filter(mode == trans_mode) %>%
+    filter(
+      if (include_zero == 1) {
+        on_time_percent >= 0
+      } else {
+        on_time_percent > 0
+      }
+    ) %>%
     mutate(on_time_percent_100 = on_time_percent * 100) %>%
     group_by(day_type, month_start) %>%
     summarize(mean_on_time_percent = mean(on_time_percent_100, na.rm = TRUE))
 }
 
 # function to return the mean bus on time percent by garage
-mean_bus_on_time_percent_by_garage <- function(dataset) {
+mean_bus_on_time_percent_by_garage <- function(dataset, include_zero) {
   dataset %>%
     filter(mode == "Bus") %>%
+    filter(
+      if (include_zero == 1) {
+        on_time_percent >= 0
+      } else {
+        on_time_percent > 0
+      }
+    ) %>%
     mutate(on_time_percent_100 = on_time_percent * 100) %>%
     group_by(current_garage, month_start) %>%
     summarize(mean_on_time_percent = mean(on_time_percent_100, na.rm = TRUE))
@@ -30,28 +52,43 @@ mean_bus_on_time_percent_by_garage <- function(dataset) {
 aggregateModuleUI <- function(id) {
   ns <- NS(id)
 
-  # Main panel for displaying outputs
-  mainPanel(
-    h4("Graph 3: Mean Bus and Light Rail On Time Percent Over Time"),
-    p("This graph is shows the difference between Bus and Light Rail on
-        time percentages."),
-    plotOutput(outputId = ns("meanOnTimePercentByMode")),
+  # Sidebar layout with input and output definitions
+  sidebarLayout(
 
-    h4("Graph 4: Mean Bus On Time Percent By Day Type"),
-    p("This graph is shows the differences in on time percentages by day type"),
-    p("The dip in SAT. and SUN. appear to correspond to the switch to Celver"),
-    plotOutput(outputId = ns("meanBusOnTimePercentByDayType")),
+    # Sidebar panel for inputs
+    sidebarPanel(
+      fluidRow(
+        column(12,
+               radioButtons(ns("include_zero"),
+                            h4("0 Percent"),
+                            choices = list("Include" = 1, "Exclude" = 0),
+                            selected = 1))
+      ),
+      width = 2
+    ),
 
-    h4("Graph 5: Mean Light Rail On Time Percent By Day Type"),
-    p("This graph is shows the differences in on time percentages by day type"),
-    plotOutput(outputId = ns("meanLightRailOnTimePercentByDayType")),
+    # Main panel for displaying outputs
+    mainPanel(
+      h4("Graph 3: Mean Bus and Light Rail On Time Percent Over Time"),
+      p("This graph is shows the difference between Bus and Light Rail on
+          time percentages."),
+      plotOutput(outputId = ns("meanOnTimePercentByMode")),
 
-    h4("Graph 6: Mean Bus On Time Percent By Current Garage"),
-    p("This graph is shows the bus differences in on time percentages
-        by garage"),
-    plotOutput(outputId = ns("meanBusOnTimePercentByGarage")),
+      h4("Graph 4: Mean Bus On Time Percent By Day Type"),
+      p("This graph is shows the differences in on time percentages by day type"),
+      p("The dip in SAT. and SUN. appear to correspond to the switch to Celver"),
+      plotOutput(outputId = ns("meanBusOnTimePercentByDayType")),
 
-    width = 12
+      h4("Graph 5: Mean Light Rail On Time Percent By Day Type"),
+      p("This graph is shows the differences in on time percentages by day type"),
+      plotOutput(outputId = ns("meanLightRailOnTimePercentByDayType")),
+
+      h4("Graph 6: Mean Bus On Time Percent By Current Garage"),
+      p("This graph is shows the bus differences in on time percentages
+          by garage"),
+      plotOutput(outputId = ns("meanBusOnTimePercentByGarage")),
+      width = 10
+    )
   )
 }
 
@@ -63,7 +100,7 @@ aggregateModuleServer <- function(id, dataset) {
       # Plot of the mean on-time percent over time vs mode
       output$meanOnTimePercentByMode <- renderPlot({
         ggplot(
-          data = mean_on_time_by_mode(dataset),
+          data = mean_on_time_by_mode(dataset, input$include_zero),
           aes(month_start, mean_on_time_percent, color = mode)) +
           geom_line(linewidth = 1) +
           ggtitle("Mean On Time Percent By Transportation Mode") +
@@ -82,7 +119,8 @@ aggregateModuleServer <- function(id, dataset) {
       # Plot of Bus mean on-time percent over time vs mode
       output$meanBusOnTimePercentByDayType <- renderPlot({
         ggplot(
-          data = mean_mode_on_time_percent_by_day_type(dataset, "Bus"),
+          data = mean_mode_on_time_percent_by_day_type(
+            dataset, "Bus", input$include_zero),
           aes(month_start, mean_on_time_percent, color = day_type)) +
           geom_line(linewidth = 1) +
           ggtitle("Mean Bus On Time Percent By Day Type") +
@@ -99,7 +137,8 @@ aggregateModuleServer <- function(id, dataset) {
       # Plot of Light Rail mean on-time percent over time vs mode
       output$meanLightRailOnTimePercentByDayType <- renderPlot({
         ggplot(
-          data = mean_mode_on_time_percent_by_day_type(dataset, "Light Rail"),
+          data = mean_mode_on_time_percent_by_day_type(
+            dataset, "Light Rail", input$include_zero),
           aes(month_start, mean_on_time_percent, color = day_type)) +
           geom_line(linewidth = 1) +
           ggtitle("Mean Light Rail On Time Percent By Day Type") +
@@ -116,7 +155,8 @@ aggregateModuleServer <- function(id, dataset) {
       # Plot of Bus mean on-time percent over time by garage
       output$meanBusOnTimePercentByGarage <- renderPlot({
         ggplot(
-          data = mean_bus_on_time_percent_by_garage(dataset),
+          data = mean_bus_on_time_percent_by_garage(
+            dataset, input$include_zero),
           aes(month_start, mean_on_time_percent, color = current_garage)) +
           geom_line(linewidth = 1) +
           ggtitle("Mean Bus On Time Percent By Garage") +
--- app/tests/testthat/test-aggregate-module.R
+++ app/tests/testthat/test-aggregate-module.R
@@ -3,36 +3,58 @@ test_dataset <- read_csv('../test_data/dataset.csv')
 # Test that the mean_on_time_by_mode returns the mean on time percent by
 # transportation mode
 test_that("the data is aggregated by route and month start", {
-  aggregate_data <- mean_on_time_by_mode(test_dataset)
+  aggregate_data <- mean_on_time_by_mode(test_dataset, 1)
   single_month <- aggregate_data %>%
-    filter(month_start == "2019-05-01")
-  start_month <- as.Date("2019-05-01")
+    filter(month_start == "2020-01-01")
+  start_month <- as.Date("2020-01-01")
   rounded_mean <- round(single_month$mean_on_time_percent, 2)
 
   expect_equal(single_month$mode, c("Bus", "Light Rail"))
   expect_equal(single_month$month_start, c(start_month, start_month))
-  expect_equal(rounded_mean, c(66.25, 80.86))
+  expect_equal(rounded_mean, c(59.02, 85.66))
+})
+
+test_that("excluding 0 percent increases mean", {
+  aggregate_data <- mean_on_time_by_mode(test_dataset, 0)
+  single_month <- aggregate_data %>%
+    filter(month_start == "2020-01-01")
+  start_month <- as.Date("2020-01-01")
+  rounded_mean <- round(single_month$mean_on_time_percent, 2)
+
+  expect_equal(rounded_mean, c(73.64, 85.66))
 })
 
 # Test that the mean_mode_on_time_percent_by_day_type
 # returns the mean Bus on time percent by day type
 test_that("the bus data is aggregated by day type and month start", {
-  aggregate_data <- mean_mode_on_time_percent_by_day_type(test_dataset, "Bus")
+  aggregate_data <- mean_mode_on_time_percent_by_day_type(
+    test_dataset, "Bus", 1)
   single_month <- aggregate_data %>%
-    filter(month_start == "2019-05-01")
-  start_month <- as.Date("2019-05-01")
+    filter(month_start == "2020-02-01")
+  start_month <- as.Date("2020-02-01")
   rounded_mean <- round(single_month$mean_on_time_percent, 2)
 
   expect_equal(single_month$day_type, c("SAT.", "SUN.", "WEEKDAY"))
   expect_equal(single_month$month_start, rep(start_month, 3))
-  expect_equal(rounded_mean, c(65.61, 70.13, 64.37))
+  expect_equal(rounded_mean, c(51.54, 49.59, 71.85))
+})
+
+test_that("excluding 0 percent increases mean", {
+  aggregate_data <- mean_mode_on_time_percent_by_day_type(
+    test_dataset, "Bus", 0)
+  single_month <- aggregate_data %>%
+    filter(month_start == "2020-02-01")
+  start_month <- as.Date("2020-02-01")
+  rounded_mean <- round(single_month$mean_on_time_percent, 2)
+  
+  expect_equal(rounded_mean, c(73.15, 73.50, 71.85))
 })
 
 # Test that the mean_mode_on_time_percent_by_day_type
 # returns the mean Light Rail on time percent by day type
 test_that("the light rail data is aggregated by day type and month start", {
   aggregate_data <- mean_mode_on_time_percent_by_day_type(
-    test_dataset, "Light Rail")
+    test_dataset, "Light Rail", 1)
   single_month <- aggregate_data %>%
     filter(month_start == "2019-05-01")
   start_month <- as.Date("2019-05-01")
@@ -46,10 +68,10 @@ test_that("the light rail data is aggregated by day type and month start", {
 # Test that the mean_bus_on_time_percent_by_garage
 # returns the mean bus on time percent by garage
 test_that("the bus data is aggregated by garage and month start", {
-  aggregate_data <- mean_bus_on_time_percent_by_garage(test_dataset)
+  aggregate_data <- mean_bus_on_time_percent_by_garage(test_dataset, 1)
   single_month <- aggregate_data %>%
-    filter(month_start == "2019-05-01")
-  start_month <- as.Date("2019-05-01")
+    filter(month_start == "2020-03-01")
+  start_month <- as.Date("2020-03-01")
   rounded_mean <- round(single_month$mean_on_time_percent, 2)
   current_garages <- c(
     "Collier",
@@ -63,7 +85,17 @@ test_that("the bus data is aggregated by garage and month start", {
   expect_equal(
     single_month$month_start, rep(start_month, 5)
   )
-  expect_equal(rounded_mean, c(70.52, 63.73, 66.95, 64.57, 67.17))
+  expect_equal(rounded_mean, c(72.82, 57.61, 74.91, 63.16, 56.58))
+})
+
+test_that("excluding 0 percent increases mean", {
+  aggregate_data <- mean_bus_on_time_percent_by_garage(test_dataset, 0)
+  single_month <- aggregate_data %>%
+    filter(month_start == "2020-03-01")
+  start_month <- as.Date("2020-03-01")
+  rounded_mean <- round(single_month$mean_on_time_percent, 2)
+
+  expect_equal(rounded_mean, c(79.29, 72.86, 74.91, 76.32, 75.09))
 })
 
 # Test server
@@ -72,6 +104,7 @@ test_that("on time percent by route plot can be accessed without error", {
     aggregateModuleServer,
     args = list(id = "aggregate-module", dataset = test_dataset),
     {
+      session$setInputs(include_zero = 1)
       expect_type(output$meanOnTimePercentByMode, "list")
     }
   )
@@ -82,6 +115,7 @@ test_that("the mean bus on time precent plot can be accessed without error", {
     aggregateModuleServer,
     args = list(id = "aggregate-module", dataset = test_dataset),
     {
+      session$setInputs(include_zero = 0)
       expect_type(output$meanBusOnTimePercentByDayType, "list")
     }
   )
@@ -92,6 +126,7 @@ test_that("the mean light rail precent plot can be accessed without error", {
     aggregateModuleServer,
     args = list(id = "aggregate-module", dataset = test_dataset),
     {
+      session$setInputs(include_zero = 1)
       expect_type(output$meanLightRailOnTimePercentByDayType, "list")
     }
   )
@@ -102,6 +137,7 @@ test_that("the mean bus by garage plot can be accessed without error", {
     aggregateModuleServer,
     args = list(id = "aggregate-module", dataset = test_dataset),
     {
+      session$setInputs(include_zero = 0)
       expect_type(output$meanBusOnTimePercentByGarage, "list")
     }
   )
